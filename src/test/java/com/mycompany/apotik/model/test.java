9641e54476b6e42c3893fe58bffd3bd2f18ccfa4
Unknown <nanangsaiful23@gmail.com>
2017-11-13 08:00:22 +0700

    test

---
+++ src/test/java/com/mycompany/apotik/model/test.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.apotik.model;
+
+/**
+ *
+ * @author nanang saiful
+ */
+
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+public class test {
+    
+}
---
+++ src/test/java/com/mycompany/apotik/model/testfeature.java
@@ -0,0 +1,15 @@
+package com.mycompany.apotik.model;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author nanang saiful
+ */
+public class testfeature {
+    
+}
