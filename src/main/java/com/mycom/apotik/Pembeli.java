f1d5b6db2db1afe341042d20cf35b41b4229e451
febrianaputriwahyuni <doniprasetyo41@gmail.com>
2017-12-18 11:33:59 +0700

    Update Pembeli.java

--- src/main/java/com/mycom/apotik/Pembeli.java
+++ src/main/java/com/mycom/apotik/Pembeli.java
@@ -4,15 +4,13 @@ public class Pembeli extends Person{
    private int id;
    private String nama; 
    private String Password;
-
+   
     public Pembeli(int id, String nama,String Password) {
         this.id = id;
         this.nama = nama;
         this.Password=Password;
     }
-
-    public Pembeli() {
-       
+    public Pembeli() {  
     }
 
     public Pembeli(String nama, String Password) {
